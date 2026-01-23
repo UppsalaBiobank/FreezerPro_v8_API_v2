74c06cd5228dda3dbc6a0e38a59da0fcc1e276bb
UBB-HenrikVestin <henrik.vestin@uppsalabiobank.uu.se>
2026-01-23 13:08:01 +0100

    Create credentials.rb
    first commit

---
+++ menu_files/credentials.rb
@@ -0,0 +1,28 @@
+module Credentials
+
+    def variable_credentials()
+        print "Enter username: "
+        $user = gets.chomp
+        while $user == ""
+            puts "No username provided"
+            puts "Enter username: "
+            $user = gets.chomp
+        end
+        #$username = "|     #{$user}                |\n"
+        print "Enter password: "
+        $password = gets.chomp
+        while $password == ""
+            puts "No password provided"
+            puts "Enter password: "
+            $password = gets.chomp
+        end
+    end
+
+    def static_credentials()
+    #static credentials should only be used for testing.
+    #change username and password to the static login/pass you want to use
+    $user = 'xx'
+    $password = 'yy'
+    end
+    extend self
+end
