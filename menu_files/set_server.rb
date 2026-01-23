56248fae720c137ebc8730201ec3ef2fd74e06c7
UBB-HenrikVestin <henrik.vestin@uppsalabiobank.uu.se>
2026-01-23 13:05:05 +0100

    Create set_server.rb
    first commit

---
+++ menu_files/set_server.rb
@@ -0,0 +1,29 @@
+require 'uri'
+module Set_Server
+
+    #def set_to_prod()        
+    #    #webadress to FP prod
+    #    #$current_server = URI.parse('https://freezerpro.regionuppsala.se')
+    #    $current_server = 'https://freezerpro.regionuppsala.se'
+    #    $menu_print = "|          #{$current_server.to_s}       |\n"
+    #end
+#
+    #def set_to_test()        
+    #    #webadress to FP test.
+    #    #$current_server = URI.parse('https://freezerpro-test.regionuppsala.se')
+    #    $current_server = 'https://freezerpro-test.regionuppsala.se'
+    #    $menu_print = "|     #{$current_server.to_s}       |\n"
+    #end
+
+    def select_server(select_value)
+        if select_value == 1
+            $current_server = 'https://freezerpro.regionuppsala.se'
+
+        elsif select_value == 2
+            $current_server = "https://freezerpro-test.regionuppsala.se"
+        
+        end
+        $menu_print = "|          #{$current_server.to_s}       |\n"
+    end
+    extend self
+end
